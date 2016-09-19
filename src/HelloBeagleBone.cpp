2e422fae128c513223e1f5d0f4c82b966c6899b5
aa <aa@aa.com>
2016-09-20 01:07:44 +0800

    multithreaded Led Blink

--- src/HelloBeagleBone.cpp
+++ src/HelloBeagleBone.cpp
@@ -10,6 +10,7 @@
 using namespace std;
 
 #include <stdio.h>
+#include <pthread.h>
 
 class Led
     {
@@ -39,10 +40,39 @@ private:
 
     };
 
+void *voidthread1( void *ptr )
+    {
+    Led l = Led("/sys/class/leds/beaglebone:green:usr3/brightness");
+    for (int ctr = 0; ctr < 5; ctr++)
+	{
+	l.TurnOffLed();
+	sleep(2);
+	l.TurnOnLed();
+	sleep(2);
+	}
+
+    }
+
+void *voidthread2( void *ptr )
+    {
+    Led l2 = Led("/sys/class/leds/beaglebone:green:usr1/brightness");
+    for (int ctr = 0; ctr < 10; ctr++)
+	{
+	l2.TurnOffLed();
+	sleep(1);
+	l2.TurnOnLed();
+	sleep(1);
+	}
+
+    }
+
+
+
 int main()
     {
     int x = 0;
 
+#if 0
     Led l = Led("/sys/class/leds/beaglebone:green:usr3/brightness");
     Led l2 = Led("/sys/class/leds/beaglebone:green:usr1/brightness");
     for (int ctr = 0; ctr < 10; ctr++)
@@ -54,6 +84,15 @@ int main()
 	l2.TurnOnLed();
 	sleep(1);
 	}
+#else
+    pthread_t thread1, thread2;
+    pthread_create( &thread1, NULL, voidthread1, NULL);
+    pthread_create( &thread2, NULL, voidthread2, NULL);
+
+
+    pthread_join( thread1, NULL);
+    pthread_join( thread2, NULL);
+#endif
 
     cout << "!!!Hello fdsfds!!!" << endl; // prints !!!Hello World!!!
     return 0;
